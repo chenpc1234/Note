aa88292617a324a3a136ae119aef06e88b828314
chenpc1234 <66248393+chenpc1234@users.noreply.github.com>
2022-03-21 18:01:48 +0800

    add by chenpc

---
+++ 数据结构/Code/src/main/java/Array/SparseArray.java
@@ -0,0 +1,78 @@
+package Array;
+
+/**
+ *
+ * 稀疏数组
+ * @author chenpc
+ * @version 1.0
+ * @since 2022/3/21/03/21  15:16
+ */
+public class SparseArray {
+
+    public static void main(String[] args) {
+        // 创建一个原始的二维数组 11 * 11
+        // 0: 表示没有棋子， 1 表示 黑子 2 表蓝子
+        final int side =11;
+        int[][] chessArr1 = new int[side][side];
+        chessArr1[1][2] = 1;
+        chessArr1[2][3] = 2;
+        chessArr1[4][5] = 2;
+        // 输出原始的二维数组
+        System.out.println("原始的二维数组~~");
+        for (int[] row : chessArr1) {
+            for (int data : row) {
+                System.out.printf("%d\t", data);
+            }
+            System.out.println();
+        }
+        // 将二维数组 转 稀疏数组的思
+        // 1. 先遍历二维数组 得到非 0 数据的个数
+        int sum = 0;
+        for (int i = 0; i < side; i++) {
+            for (int j = 0; j < side; j++) {
+                if (chessArr1[i][j] != 0) {
+                    sum++;
+                }
+            }
+        }
+        // 2. 创建对应的稀疏数组
+        int[][] sparseArr = new int[sum + 1][3];
+        // 给稀疏数组赋值
+        sparseArr[0][0] = side;
+        sparseArr[0][1] = side;
+        sparseArr[0][2] = sum;
+        // 遍历二维数组，将非 0 的值存放到 sparseArr 中
+        //count 用于记录是第几个非 0 数据
+        int count = 0;
+        for (int i = 0; i < side; i++) {
+            for (int j = 0; j < side; j++) {
+                if (chessArr1[i][j] != 0) {
+                    count++;
+                    sparseArr[count][0] = i;
+                    sparseArr[count][1] = j;
+                    sparseArr[count][2] = chessArr1[i][j];
+                }
+            }
+        }
+        // 输出稀疏数组的形式
+        System.out.println("得到稀疏数组为~~~~");
+        for (int[] ints : sparseArr) {
+            System.out.printf("%d\t%d\t%d\t\n", ints[0], ints[1], ints[2]);
+        }
+        //将稀疏数组 --》 恢复成 原始的二维数组
+        //1. 先读取稀疏数组的第一行，根据第一行的数据，创建原始的二维数组，比如上面的 chessArr2 = int [side][side]
+        int[][] chessArr2 = new int[sparseArr[0][0]][sparseArr[0][1]];
+        //2. 在读取稀疏数组后几行的数据(从第二行开始)，并赋给 原始的二维数组 即可
+        for (int i = 1; i < sparseArr.length; i++) {
+            chessArr2[sparseArr[i][0]][sparseArr[i][1]] = sparseArr[i][2];
+        }
+        // 输出恢复后的二维数组
+        System.out.println("恢复后的二维数组");
+        for (int[] row : chessArr2) {
+            for (int data : row) {
+                System.out.printf("%d\t", data);
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ 数据结构/Code/src/main/java/Queue/ArrayQueueDemo.java
@@ -0,0 +1,118 @@
+package Queue;
+
+import java.awt.print.Printable;
+
+/**
+ * @author chenpc
+ * @version 1.0
+ * @since 2022/3/21/03/21  16:15
+ */
+public class ArrayQueueDemo {
+    public static void main(String[] args) {
+        ArrayQueue arrayQueue = new ArrayQueue(10);
+        arrayQueue.add(1);
+        arrayQueue.add(2);
+        arrayQueue.add(3);
+        arrayQueue.add(4);
+        arrayQueue.add(5);
+        arrayQueue.add(6);
+        arrayQueue.add(7);
+        arrayQueue.add(8);
+        arrayQueue.add(9);
+        arrayQueue.add(0);
+        System.out.println(arrayQueue.remove());
+        arrayQueue.add(11);
+        System.out.println(arrayQueue.front);
+        System.out.println(arrayQueue.rear);
+        arrayQueue.showQueue();
+        System.out.println();
+        System.out.println(arrayQueue.size());
+    }
+
+    /**
+     * 环形数组模拟队列
+     * 尾针和头针不能指向数组同一下标，保证环形数组有方向。尾针节点的数据为无效数据
+     * 添加数据时，尾针处添加数据，同时尾针后移
+     */
+    static class ArrayQueue {
+        private int maxSize; //队列容量
+        private int front; // 队列头针
+        private int rear;   //队列尾针
+        private int[] data; //数组存放数据
+
+        // 有参构造器
+        public ArrayQueue(int size) {
+            maxSize = size + 1;
+            data = new int[maxSize];
+            front = 0;
+            rear = 0;
+        }
+
+        //判断队列是否满了
+        public boolean isFull() {
+            return (rear + 1) % maxSize == front;
+        }
+
+        //判断队列是否为空
+        public boolean isEmpty() {
+            return front == rear; //队列头指针 = 队列尾指针
+        }
+
+        //添加数据
+        public void add(int value) {
+            if (isFull()) {
+                throw new RuntimeException("队列满了");
+            }
+            data[rear] = value;
+            rear = (rear + 1) % maxSize;
+        }
+
+        //获取并移除队列头数据
+        public int remove() {
+            if (isEmpty()) {
+                throw new RuntimeException("队列为空");
+            }
+            int temp = data[front];
+            front = (front + 1) % maxSize;
+            return temp;
+        }
+
+        //获取队列头数据
+        public int element() {
+            if (isEmpty()) {
+                throw new RuntimeException("队列为空");
+            }
+            return data[front];
+        }
+
+        //获取队列长度
+        public int size() {
+//            if (front <= rear){
+//                return rear-front;
+//            }else{
+//                return rear+maxSize-front;
+//            }
+            return (rear + maxSize - front) % maxSize;
+        }
+
+        //展示队列数据
+        public void showQueue() {
+            if (isEmpty()) {
+                throw new RuntimeException("队列为空");
+            }
+            if (front < rear) {
+                for (int i = front; i < rear; i++) {
+                    System.out.print(data[i] + "\t");
+                }
+            } else {
+                for (int i = front; i < maxSize; i++) {
+                    System.out.print(data[i] + "\t");
+                }
+                for (int i = 0; i < rear; i++) {
+                    System.out.print(data[i] + "\t");
+                }
+            }
+        }
+
+    }
+}
