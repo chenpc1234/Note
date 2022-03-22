eaa98857f04bb636faab722e0e0587592f26ca90
chenpc1234 <66248393+chenpc1234@users.noreply.github.com>
2022-03-22 17:43:37 +0800

    链表结构-单链表

--- 数据结构/Code/src/main/java/LinkedList/SingleLinkedListDemo.java
+++ 数据结构/Code/src/main/java/LinkedList/SingleLinkedListDemo.java
@@ -8,71 +8,92 @@ package LinkedList;
 public class SingleLinkedListDemo {
     public static void main(String[] args) {
         SingleLinkedList singleLinkedList = new SingleLinkedList();
-        singleLinkedList.addByScore(new Node(1,111));
+        singleLinkedList.addByScore(new Node(1, 111));
 
-        singleLinkedList.addByScore(new Node(3,333));
-        singleLinkedList.add(new Node(4,444));
-        singleLinkedList.addByScore(new Node(5,555));
-        singleLinkedList.add(new Node(12,222));
-        singleLinkedList.addByScore(new Node(12,222));
+        singleLinkedList.addByScore(new Node(3, 333));
+        singleLinkedList.add(new Node(4, 444));
+        singleLinkedList.addByScore(new Node(5, 555));
+        singleLinkedList.add(new Node(12, 222));
+        singleLinkedList.update(new Node(12, 88888));
+        singleLinkedList.addByScore(new Node(12, 222));
 
     }
 }
 
-class SingleLinkedList{
+class SingleLinkedList {
     /**
-     *定义头节点
+     * 定义头节点
      */
-    Node head =new Node(0,null);
+    Node head = new Node(0, null);
 
     /**
      * 添加节点
+     *
      * @param node 节点
      */
-    public void add(Node node){
-        Node temp =head;
-        while (temp.next!=null){
-            temp=temp.next;
+    public void add(Node node) {
+        Node temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
         }
-        temp.next=node;
+        temp.next = node;
     }
+
     /**
-     * 根据分值添加节点,但是必须保证节点分数是有序的
+     * 根据排序号添加节点,但是必须保证节点分数是有序的
+     *
      * @param node 节点
      */
-    public void addByScore(Node node){
+    public void addByScore(Node node) {
         boolean flag = true;
         //判断节点是否有序
-        Node tempNode =head;
+        Node tempNode = head;
         int order = 0;
         while (tempNode.next != null) {
-            if (tempNode.next.order>order){
-                tempNode=tempNode.next;
+            if (tempNode.next.order > order) {
+                tempNode = tempNode.next;
                 order = tempNode.order;
-            }else {
+            } else {
                 throw new RuntimeException("列表不是有序的");
             }
 
         }
 
 
-        Node temp =head;
-        while (temp.next!=null){
-            if (node.order == temp.next.order){
-                flag =false;
+        Node temp = head;
+        while (temp.next != null) {
+            if (node.order == temp.next.order) {
+                flag = false;
                 break;
             }
-            if (node.order < temp.next.order){
+            if (node.order < temp.next.order) {
                 break;
             }
-            temp=temp.next;
+            temp = temp.next;
         }
-        if (flag){
-            node.next= temp.next;
-            temp.next=node;
-        }else{
-            System.out.println("分值重复，不能插入"+node);
+        if (flag) {
+            node.next = temp.next;
+            temp.next = node;
+        } else {
+            System.out.println("分值重复，不能插入" + node);
         }
+    }
+
+    /**
+     * 根据排序更新节点信息
+     * @param node 节点新数据
+     */
+    public void update(Node node) {
+        Node tempNode = head;
+        while (tempNode.next != null) {
+            if (tempNode.next.order == node.order) {
+                tempNode.next.data =node.data;
+                System.out.println("更新成功");
+                return;
+            }
+            tempNode=tempNode.next;
+        }
+        System.out.println("更新失败");
     }
 
 }
@@ -80,13 +101,14 @@ class SingleLinkedList{
 /**
  * 定义一个带排序的节点(也可不带)
  */
-class Node{
+class Node {
     int order;
     Object data;
     Node next;
-    Node(int order,Object data){
-        this.order=order;
-        this.data =data;
+
+    Node(int order, Object data) {
+        this.order = order;
+        this.data = data;
     }
 
     @Override
