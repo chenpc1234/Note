9e6da3c141bbf898edcf60e968b21b18c09d3031
chenpc1234 <66248393+chenpc1234@users.noreply.github.com>
2022-05-06 11:21:08 +0800

    笔记整理

---
+++ SpringSecurity/code/src/main/java/com/chenpc/security/config/SecurityConfig.java
@@ -0,0 +1,6 @@
+package com.chenpc.security.config;/**
+ * @author chenpc
+ * @since 2022/5/2/05/02  14:20
+ * @version 1.0
+ */public class SecurityConfig {
+}
---
+++ SpringSecurity/code/src/main/java/com/chenpc/security/controller/TestController.java
@@ -0,0 +1,6 @@
+package com.chenpc.security.controller;/**
+ * @author chenpc
+ * @since 2022/5/2/05/02  14:14
+ * @version 1.0
+ */public class TestController {
+}
